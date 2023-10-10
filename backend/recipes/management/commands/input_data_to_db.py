cb27d03cd1b1af376ef4e4831cf811c583fa3d18
Алексей Алексеев <alekseev0409@gmail.com>
2023-10-10 11:05:48 +0300

    ghhhh

--- backend/recipes/management/commands/input_data_to_db.py
+++ backend/recipes/management/commands/input_data_to_db.py
@@ -30,6 +30,5 @@ class Command(BaseCommand):
                 slug=elem["slug"],
             )
         self.stdout.write(
-            self.style.SUCCESS(
-            f"В базу добавлено {counter} ингредиентов и теги")
+            self.style.SUCCESS(f"В базу добавлено {counter} ингредиентов и теги")
         )
