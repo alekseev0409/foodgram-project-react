042914953c91af7b829b1175158eefd8a848691a
Алексей Алексеев <alekseev0409@gmail.com>
2023-10-10 10:23:09 +0300

    ggggg

--- backend/recipes/management/commands/input_data_to_db.py
+++ backend/recipes/management/commands/input_data_to_db.py
@@ -1,17 +1,3 @@
-import json
-
-from django.core.management.base import BaseCommand
-
-from recipes.models import Ingredient, Tag
-
-
-tag = [
-    {"name": "завтрак", "color": "#CC5500", "slug": "breakfast"},
-    {"name": "обед", "color": "#006633", "slug": "lunch"},
-    {"name": "ужин", "color": "#660099", "slug": "dinner"},
-]
-
-
 class Command(BaseCommand):
     def handle(self, *args, **options):
         Ingredient.objects.all().delete()
@@ -20,7 +6,8 @@ class Command(BaseCommand):
             counter = 0
             for row in reader:
                 Ingredient.objects.create(
-                    name=row["name"], measurement_unit=row["measurement_unit"]
+                    name=row["name"],
+                    measurement_unit=row["measurement_unit"]
                 )
                 counter += 1
         for elem in tag:
@@ -31,5 +18,6 @@ class Command(BaseCommand):
             )
         self.stdout.write(
             self.style.SUCCESS(
-            f"В базу добавлено {counter} ингредиентов и теги")
+                f"В базу добавлено {counter} ингредиентов и теги"
+            )
         )
